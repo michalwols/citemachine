70f83dddc425e2ea24793d2c58179f7294256685
michalwols <michalwols@gmail.com>
2014-04-22 13:34:12 -0400

    remove main function from util.py

--- citemachine/util.py
+++ citemachine/util.py
@@ -1,5 +1,4 @@
 import nltk
-import gensim
 from collections import defaultdict
 
 
@@ -16,10 +15,10 @@ def count_words(word_list, counts_dict=None):
 
 def filter_dict(func, dictionary):
     """Filter a dictionary *in place* based on filter function
-    
+
     Args:
-        func: func(key, value), returns ture if item should be retained, 
-            false otherwise 
+        func: func(key, value), returns ture if item should be retained,
+            false otherwise
         dictionary: dict to be filtered
     """
     for key in dictionary.keys():
@@ -50,26 +49,3 @@ class BiDirMap(object):
 
     def get_value(self, key):
         return self.key_to_val(key)
-
-
-def main():
-
-    corpus = nltk.corpus.reuters
-    docs = corpus.fileids()
-    words = corpus.words()
-
-    stemmer = nltk.stem.lancaster.LancasterStemmer()
-    words = stem_all(words, stemmer)
-
-    word_counts = count_words(words)
-
-    stopwords = set(nltk.corpus.stopwords.words('english'))
-
-    is_valid = lambda k, v: not ((v < 3) or (len(k) < 3) or (k in stopwords))
-    word_counts = filter_dict(is_valid, word_counts)
-
-    return word_counts
-
-
-if __name__ == '__main__':
-    main()
